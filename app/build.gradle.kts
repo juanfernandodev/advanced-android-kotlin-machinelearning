8fea2f3589293ebdd91cc1198c001e63f1ac2709
Juan Fernando Romero <jfromero1367@gmail.com>
2024-03-01 15:17:50 -0500

    Add retrofit dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,13 +42,22 @@ android {
 dependencies {
     val lifecycle_version = "2.7.0"
     val activity_version = "1.8.2"
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    val retrofit_version = "2.9.0"
+    val android_core_version = "1.12.0"
+    val android_appcompat_version = "1.6.1"
+    val android_material = "1.11.0"
+    val constrain_layout_version = "2.1.4"
+    val junit_version = "4.13.2"
+    implementation("androidx.core:core-ktx:$android_core_version")
+    implementation("androidx.appcompat:appcompat:$android_appcompat_version")
+    implementation("com.google.android.material:material:$android_material")
+    implementation("androidx.constraintlayout:constraintlayout:$constrain_layout_version")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
     implementation("androidx.activity:activity-ktx:$activity_version")
-    testImplementation("junit:junit:4.13.2")
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
+    implementation("com.squareup.retrofit2:converter-moshi:$retrofit_version")
+    testImplementation("junit:junit:$junit_version")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
