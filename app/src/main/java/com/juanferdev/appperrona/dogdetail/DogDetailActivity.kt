413eb20110fa592ac9298af22afb596499546a46
Juan Fernando Romero <jfromero1367@gmail.com>
2024-03-04 14:00:59 -0500

    Adding Layout by DogDetailActivity, themes.xml and colors.xml.

---
+++ app/src/main/java/com/juanferdev/appperrona/dogdetail/DogDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.juanferdev.appperrona.dogdetail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.juanferdev.appperrona.R
+
+class DogDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dog_detail)
+    }
+}
