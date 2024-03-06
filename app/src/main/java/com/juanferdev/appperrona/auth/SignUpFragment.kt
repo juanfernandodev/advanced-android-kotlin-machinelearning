49502b4f4271676ccd01176675d80821e1e0127b
juanfernandoro@ufps.edu.co <juanfernandoro@ufps.edu.co>
2024-03-06 12:04:48 -0500

    Validating fields on SignUpFragment

--- app/src/main/java/com/juanferdev/appperrona/auth/SignUpFragment.kt
+++ app/src/main/java/com/juanferdev/appperrona/auth/SignUpFragment.kt
@@ -1,20 +1,64 @@
 package com.juanferdev.appperrona.auth
 
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.juanferdev.appperrona.R
 import com.juanferdev.appperrona.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignUpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = FragmentSignUpBinding.inflate(inflater)
+        binding = FragmentSignUpBinding.inflate(inflater)
+        setupSignUpButton()
         return binding.root
+    }
+
+    private fun setupSignUpButton() {
+        binding.signUpButton.setOnClickListener {
+            validateFields()
+        }
+    }
+
+    private fun validateFields() {
+        binding.emailInput.error = String()
+        binding.passwordInput.error = String()
+        binding.confirmPasswordInput.error = String()
+
+        val email = binding.emailEdit.text.toString()
+        if (!isValidEmail(email)) {
+            binding.emailInput.error = getString(R.string.email_is_not_valid)
+            return
+        }
+        val password = binding.passwordEdit.text.toString()
+        if (password.isBlank()) {
+            binding.passwordInput.error = getString(R.string.password_must_not_be_empty)
+            return
+        }
+
+        val passwordConfirmation = binding.confirmPasswordEdit.text.toString()
+        if (passwordConfirmation.isBlank()) {
+            binding.confirmPasswordInput.error = getString(R.string.password_must_not_be_empty)
+            return
+        }
+
+        if (password != passwordConfirmation) {
+            binding.confirmPasswordInput.error = getString(R.string.password_do_not_match)
+            return
+        }
+    }
+
+    private fun isValidEmail(email: String): Boolean {
+        return email.isNotBlank()
+                && Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 
 
