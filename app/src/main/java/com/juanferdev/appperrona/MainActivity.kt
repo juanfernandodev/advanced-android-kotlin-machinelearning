32f5e9990f08e9c96d1fecd76ebbb482d21a0a5f
juanfernandoro@ufps.edu.co <juanfernandoro@ufps.edu.co>
2024-03-08 14:32:24 -0500

    Opening DogListActivity from MainActivity.

--- app/src/main/java/com/juanferdev/appperrona/MainActivity.kt
+++ app/src/main/java/com/juanferdev/appperrona/MainActivity.kt
@@ -5,23 +5,37 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.juanferdev.appperrona.auth.LoginActivity
 import com.juanferdev.appperrona.databinding.ActivityMainBinding
+import com.juanferdev.appperrona.doglist.DogListActivity
 import com.juanferdev.appperrona.models.User
 import com.juanferdev.appperrona.settings.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         validateUser()
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.settingsFab.setOnClickListener {
-            openSettingsActivity()
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        initClicks()
         setContentView(binding.root)
     }
 
-    private fun openSettingsActivity() {
-        startActivity(Intent(this, SettingsActivity::class.java))
+    private fun initClicks() {
+        binding.settingsFab.setOnClickListener {
+            openSettingsActivity()
+        }
+
+        binding.dogListFab.setOnClickListener {
+            openDogListActivity()
+        }
     }
+
+    private fun openSettingsActivity() =
+        startActivity(Intent(this, SettingsActivity::class.java))
+
+
+    private fun openDogListActivity() =
+        startActivity(Intent(this, DogListActivity::class.java))
 
     private fun validateUser() {
         val user = User.getLoggedInUser(this)
