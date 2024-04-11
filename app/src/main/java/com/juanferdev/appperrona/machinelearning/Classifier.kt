afafa88e7d9fc6542f26cb90bb33d4042dca30f6
Juan Fernando Romero <jfromero1367@gmail.com>
2024-04-11 11:47:49 -0500

    Moving method in Classifier in order to create test

--- app/src/main/java/com/juanferdev/appperrona/machinelearning/Classifier.kt
+++ app/src/main/java/com/juanferdev/appperrona/machinelearning/Classifier.kt
@@ -1,7 +1,14 @@
 package com.juanferdev.appperrona.machinelearning
 
+import android.annotation.SuppressLint
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageFormat
+import android.graphics.Rect
+import android.graphics.YuvImage
+import androidx.camera.core.ImageProxy
 import com.juanferdev.appperrona.MAX_RECOGNITION_DOG_RESULTS
+import java.io.ByteArrayOutputStream
 import java.nio.MappedByteBuffer
 import java.util.PriorityQueue
 import javax.inject.Inject
@@ -127,5 +134,35 @@ class Classifier @Inject constructor(
         }
 
         return recognitions
+    }
+
+    @SuppressLint("UnsafeOptInUsageError")
+    fun convertImageProxyToBitmap(imageProxy: ImageProxy): Bitmap? {
+        val image = imageProxy.image ?: return null
+
+        val yBuffer = image.planes[0].buffer // Y
+        val uBuffer = image.planes[1].buffer // U
+        val vBuffer = image.planes[2].buffer // V
+
+        val ySize = yBuffer.remaining()
+        val uSize = uBuffer.remaining()
+        val vSize = vBuffer.remaining()
+
+        val nv21 = ByteArray(ySize + uSize + vSize)
+
+        //U and V are swapped
+        yBuffer.get(nv21, 0, ySize)
+        vBuffer.get(nv21, ySize, vSize)
+        uBuffer.get(nv21, ySize + vSize, uSize)
+
+        val yuvImage = YuvImage(nv21, ImageFormat.NV21, image.width, image.height, null)
+        val out = ByteArrayOutputStream()
+        yuvImage.compressToJpeg(
+            Rect(0, 0, yuvImage.width, yuvImage.height), 100,
+            out
+        )
+        val imageBytes = out.toByteArray()
+
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
     }
 }
--- app/src/main/java/com/juanferdev/appperrona/machinelearning/ClassifierRepository.kt
+++ app/src/main/java/com/juanferdev/appperrona/machinelearning/ClassifierRepository.kt
@@ -1,13 +1,6 @@
 package com.juanferdev.appperrona.machinelearning
 
-import android.annotation.SuppressLint
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.ImageFormat
-import android.graphics.Rect
-import android.graphics.YuvImage
 import androidx.camera.core.ImageProxy
-import java.io.ByteArrayOutputStream
 import javax.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
@@ -19,42 +12,11 @@ class ClassifierRepository @Inject constructor(
 
     override suspend fun recognizedImage(imageProxy: ImageProxy) =
         withContext(dispatcherIO) {
-            val bitmap = convertImageProxyToBitmap(imageProxy)
+            val bitmap = classifier.convertImageProxyToBitmap(imageProxy)
             if (bitmap == null) {
                 DogRecognition()
             } else {
                 classifier.recognizeImage(bitmap).first()
             }
         }
-
-
-    @SuppressLint("UnsafeOptInUsageError")
-    private fun convertImageProxyToBitmap(imageProxy: ImageProxy): Bitmap? {
-        val image = imageProxy.image ?: return null
-
-        val yBuffer = image.planes[0].buffer // Y
-        val uBuffer = image.planes[1].buffer // U
-        val vBuffer = image.planes[2].buffer // V
-
-        val ySize = yBuffer.remaining()
-        val uSize = uBuffer.remaining()
-        val vSize = vBuffer.remaining()
-
-        val nv21 = ByteArray(ySize + uSize + vSize)
-
-        //U and V are swapped
-        yBuffer.get(nv21, 0, ySize)
-        vBuffer.get(nv21, ySize, vSize)
-        uBuffer.get(nv21, ySize + vSize, uSize)
-
-        val yuvImage = YuvImage(nv21, ImageFormat.NV21, image.width, image.height, null)
-        val out = ByteArrayOutputStream()
-        yuvImage.compressToJpeg(
-            Rect(0, 0, yuvImage.width, yuvImage.height), 100,
-            out
-        )
-        val imageBytes = out.toByteArray()
-
-        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
-    }
 }
